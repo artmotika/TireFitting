86d821408fe388d4dfcee65e57df0c97df2b40b2
Motika Artem <artmotika@gmail.com>
2024-05-23 05:59:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
+import { useGoogleLogin } from '@react-oauth/google';
 
 function App() {
   return (
     <div className="App">
       <h1>Login Google</h1>
-      <GoogleLogin
+      const login = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+      });
+      <MyCustomButton onClick={() => login()}>Sign in with Google 🚀</MyCustomButton>;
+      {/* <GoogleLogin
         onSuccess={credentialResponse => {
           const credentialResponseDecoded = jwtDecode(
             credentialResponse.credential
@@ -17,7 +22,7 @@ function App() {
         onError={() => {
           console.log('Login Failed');
         }}
-      />
+      /> */}
     </div>
   );
 }
